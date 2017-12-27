11253462761cf5ac891d8a791bee052f0c3595d5
Madan Narra <narra.madan@outlook.com>
2017-12-27 20:55:00 +0530

    Added vue-frontend with sample login and few routes

---
+++ vue-frontend/src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container fill-height class="ma-0 pa-0">
+        <v-layout class="ma-0 pa-0" row wrap>
+            <v-flex md10 class="ma-0 pa-0">
+                <div class="h-100" style='background-image: url("/static/images/bg.jpg");'></div>
+            </v-flex>
+            <v-flex md2 class="h-100">
+                <v-container fill-height>
+                    <v-layout row wrap align-center>
+                        <v-flex class="text-xs-center">
+                            <v-card>
+                                <v-card-title primary-title>
+                                    <div>
+                                        <h3 class="headline mb-0">Login</h3>                        
+                                    </div>
+                                    <v-container grid-list-md>
+                                        <v-layout wrap>
+                                            <v-flex xs12 sm12 md12>
+                                                <v-text-field label="Username" required></v-text-field>
+                                            </v-flex>
+                                            <v-flex xs12 sm12 md12>
+                                                <v-text-field label="Password" type="password" required></v-text-field>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-container>
+                                </v-card-title>
+                                <v-card-actions>
+                                    <v-btn round color="primary" to="/">Login</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-container>                
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data () {
+      return {
+      }
+    }
+}
+</script>
+
+<style>
+.h-100  { height: 100%; }
+</style>
